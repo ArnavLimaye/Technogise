6e963aab10d29d12cfaf9e9752a535eb19e1ef92
arnavlimaye <aal4493@gmail.com>
2020-02-05 16:27:35 +0530

    1. Added NewExecutor and Validator classes in ChessMoves 2. Added ValidatorTests class in ChessMoves.Tests to test Validator class 3. All tests in ValidatorTests are currently failing AS EXPECTED

---
+++ ChessMoves/ChessMoves/NewExecutor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("ChessMoves.Tests")]
+namespace ChessMoves
+{
+    internal class NewExecutor
+    {
+        /***public static void Main(string[] args)
+        {
+            string userInput;
+            Initializer.Initialize();
+
+            Console.WriteLine("Enter the input");
+            userInput = Console.ReadLine();
+
+            ChessBoard chessBoard = new ChessBoard();
+
+            Validator.ValidateInput(userInput);
+            
+            //string outputString = CreateDisplayStringFromCells(givenPiece.allowedCells);
+
+            //Console.WriteLine(outputString);
+            Console.ReadKey();
+        }***/
+    }
+}
---
+++ ChessMoves/ChessMoves/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("ChessMoves.Tests")]
+namespace ChessMoves
+{
+    internal static class Validator
+    {
+        internal static string InvalidInputError = "Invalid Input : No Space Between Piece Name and Initial Cell";
+        internal static string InvalidNameError = "Incorrect Piece Name";
+        internal static string InvalidColumnName = "Invalid Column Name";
+        internal static string InvalidRowNumber = "Invalid Row Number";
+
+        public static void ValidateInput(string userInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void ValidateInputName(string userInputName)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void ValidateInputCell(string userInputCell)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
