e8626a279c82f2aab2f8cd3d1af9847ef5785f8e
arnavlimaye <aal4493@gmail.com>
2020-02-05 17:34:52 +0530

    1. Added Mapper class to map pieceName to the required object and return that object 2. Added tests corresponding to Mapper functionality 3. TestStatus - Tests passing successfully

---
+++ ChessMoves/ChessMoves.Tests/RedesignTests/MapperTests.cs
@@ -0,0 +1,27 @@
+﻿using ChessMoves.PieceClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChessMoves.Tests.RedesignTests
+{
+    public class MapperTests
+    {
+        [Theory]
+        [InlineData("Rook",typeof(Rook))]
+        [InlineData("pawn", typeof(Pawn))]
+        public void ObjectInstanceOfRightTypeShouldBeReturnedByMapper(string pieceName,Type expectedType)
+        {
+            //Arrange
+
+            //Act
+            var piece = Mapper.GetRequiredChessPiece(pieceName);
+
+            //Assert
+            Assert.IsType(expectedType, piece);
+        }
+    }
+}
---
+++ ChessMoves/ChessMoves/PieceClasses/Rook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMoves.PieceClasses
+{
+    internal class Rook : Piece
+    {
+        internal override void SearchForAllPossibleMoves(ChessBoard chessBoard)
+        {
+            SearchForAllPossibleHorizontalMoves(chessBoard);
+            SearchForAllPossibleVerticalMoves(chessBoard);
+        }
+    }
+}
