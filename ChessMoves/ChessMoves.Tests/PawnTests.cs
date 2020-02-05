7079b2f2476b1406c1ddc76b813f968bfc4969a3
ArnavLimaye <aal4493@gmail.com>
2020-02-05 11:25:34 +0530

    1. Created Pawn class 2. Wrote tests for Pawn. TestStatus - success 3.

---
+++ ChessMoves/ChessMoves.Tests/PawnTests.cs
@@ -0,0 +1,53 @@
+﻿using ChessMoves.PieceClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChessMoves.Tests
+{
+    public class PawnTests
+    {
+        ChessBoard chessBoard = new ChessBoard();
+
+        [Theory]
+        [InlineData("C6",3,7)]
+        public void OnlyForwardVerticalCellShouldBeReturned(string input,int outputColumn, int outputRow)
+        {
+            //Arrange
+            Initializer.Initialize();
+            List<Cell> expectedCells = new List<Cell> { new Cell { row = outputRow, column = outputColumn } };
+            Pawn pawn = new Pawn();
+
+            //Act
+            pawn.MapUserInputCellToChessPieceInitialRowColumn(input);
+            pawn.SearchForAllPossibleMoves(chessBoard);
+
+            //Assert
+            Assert.True(AreTwoListsEquivalent(expectedCells,pawn.allPossibleMoves));
+        }
+
+        [Theory]
+        [InlineData("D8")]
+        public void NoMovesShouldBeReturnedIfPawnIsAtTopmostRow(string input)
+        {
+            //Arrange
+            Initializer.Initialize();
+            Pawn pawn = new Pawn();
+
+            //Act
+            pawn.MapUserInputCellToChessPieceInitialRowColumn(input);
+            pawn.SearchForAllPossibleMoves(chessBoard);
+
+            //Assert
+            Assert.Empty(pawn.allPossibleMoves);
+        }
+
+        private bool AreTwoListsEquivalent(List<Cell> expectedOutputCells, List<Cell> actualOutputCells)
+        {
+            return (expectedOutputCells.All(actualOutputCells.Contains) && expectedOutputCells.Count == actualOutputCells.Count);
+        }
+    }
+}
---
+++ ChessMoves/ChessMoves/PieceClasses/Pawn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("ChessPiece.Tests")]
+namespace ChessMoves.PieceClasses
+{
+    internal class Pawn : Piece
+    {
+        internal Pawn()
+        {
+            initialCell = new Cell();
+            allPossibleMoves = new List<Cell>();
+        }
+
+        internal override void SearchForAllPossibleMoves(ChessBoard chessboard)
+        {
+            AddCell(initialCell.row + 1, initialCell.column, chessboard);
+        }
+    }
+}
